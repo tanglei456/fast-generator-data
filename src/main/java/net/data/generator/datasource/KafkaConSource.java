da870babe7bcca86aa61a18c0bdd7a78118f7e08
tanglei <1257749237@qq.com>
2023-02-16 19:57:43 +0800

    删除冗余代码

--- src/main/java/net/data/generator/datasource/KafkaConSource.java
+++ src/main/java/net/data/generator/datasource/KafkaConSource.java
@@ -1,6 +1,6 @@
 package net.data.generator.datasource;
 
-import cn.hutool.extra.spring.SpringUtil;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
